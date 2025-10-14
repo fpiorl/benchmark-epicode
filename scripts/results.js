c95afd4bf714f9cf2ab98395356499d7a2a60b36
pask517 <andreapascarella99@gmail.com>
2025-10-14 13:04:04 +0200

    js modificato

--- scripts/results.js
+++ scripts/results.js
@@ -1,9 +1,7 @@
 const ctx = document.getElementById("myChart")
 let correctAnswersPercent = 80
 let wrongAnswersPercent = 100 - correctAnswersPercent
-const ctx = document.getElementById("myChart");
-let correctAnswers = 60;
-let wrongAnswers = 40;
+
 const config = {
   type: "doughnut",
 
@@ -34,5 +32,5 @@ const config = {
     },
     cutout: "70%",
   },
-};
-myChart = new Chart(ctx, config);
+}
+myChart = new Chart(ctx, config)

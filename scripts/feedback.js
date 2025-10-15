928befe470dd41155f114c567b4d0b2c6a0a7413
Francesco Pio Orlando <franzesqo@gmail.com>
2025-10-15 15:19:17 +0200

    feedback refactor

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,31 +1,20 @@
 let litStars = 0;
+
 const stars = document.querySelectorAll(".star");
 stars.forEach((stella, i) => {
-  stella.addEventListener("mouseenter", (event) => {
-    console.log(stella.firstElementChild);
-    stella.firstElementChild.style.fill = "#00FFFF";
-    for (let j = 0; j <= i; j++) {
-      stars[j].firstElementChild.style.fill = "#00FFFF";
-    }
-    for (let j = i + 1; j < stars.length; j++) {
-      stars[j].firstElementChild.style.fill = "black";
-    }
-  });
-  stella.addEventListener("click", () => {
-    litStars = i;
-  });
-  stella.parentElement.addEventListener("mouseleave", () => {
-    if (litStars === 0) {
-      for (let j = 0; j < stars.length; j++) {
-        stars[j].firstElementChild.style.fill = "black";
-      }
-      return;
-    }
-    for (let j = 0; j < stars.length; j++) {
-      stars[j].firstElementChild.style.fill = "black";
-    }
-    for (let j = 0; j <= litStars; j++) {
-      stars[j].firstElementChild.style.fill = "#00FFFF";
-    }
-  });
+    stella.addEventListener("mouseenter", (event) => {
+        illumina(i+1)
+    });
+    stella.addEventListener("click", () => {
+        litStars = i+1;
+    });
+    stella.parentElement.addEventListener("mouseleave", () => {
+        illumina(litStars)
+    });
 });
+
+const illumina = function(numero){
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].firstElementChild.style.fill = ( i >= numero )? "black":"#00FFFF";
+    }
+}

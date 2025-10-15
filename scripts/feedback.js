b63b2c881b20267d9c6e10af9d77962f4a901ebc
Hammers <gianluca.hammers@gmail.com>
2025-10-15 15:38:14 +0200

    improved css

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -2,19 +2,21 @@ let litStars = 0;
 
 const stars = document.querySelectorAll(".star");
 stars.forEach((stella, i) => {
-    stella.addEventListener("mouseenter", (event) => {
-        illumina(i+1)
-    });
-    stella.addEventListener("click", () => {
-        litStars = i+1;
-    });
-    stella.parentElement.addEventListener("mouseleave", () => {
-        illumina(litStars)
-    });
+  stella.addEventListener("mouseenter", () => {
+    illumina(i + 1);
+  });
+  stella.addEventListener("click", () => {
+      litStars = i + 1;
+      
+  });
+  stella.parentElement.addEventListener("mouseleave", () => {
+    illumina(litStars);
+  });
 });
 
-const illumina = function(numero){
-    for (let i = 0; i < stars.length; i++) {
-        stars[i].firstElementChild.style.fill = ( i >= numero )? "black":"#00FFFF";
-    }
-}
+const illumina = function (numero) {
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].firstElementChild.style.fill =
+      i >= numero ? "rgb(11,17,59)" : "#00FFFF";
+  }
+};

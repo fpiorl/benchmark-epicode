286e8fd5b1c9c271b3cfab761ae679c466e934ca
Francesco Pio Orlando <franzesqo@gmail.com>
2025-10-15 15:46:25 +0200

    added feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -3,10 +3,15 @@ let litStars = 0;
 const stars = document.querySelectorAll(".star");
 stars.forEach((stella, i) => {
     stella.addEventListener("mouseenter", (event) => {
-        illumina(i+1)
+        illumina(litStars > 0? litStars: i+1)
     });
     stella.addEventListener("click", () => {
-        litStars = i+1;
+        if (litStars === i+1){
+            litStars = 0
+            illumina(0) 
+        } else {
+            litStars = i+1;
+        }
     });
     stella.parentElement.addEventListener("mouseleave", () => {
         illumina(litStars)
